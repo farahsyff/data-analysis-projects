fcf0884b96133262c725b023db87310c64d13cdf
Farah Fajriah <farahsyff.work@gmail.com>
2025-04-05 22:22:11 +0700

    Update sales_customer_analysis.sql
    Added two new analysis queries:
    - statistical information regarding customer's age (min, max, mean, median, etc)
    - total sales by customer city

--- sql/sales_customer_analysis.sql
+++ sql/sales_customer_analysis.sql
@@ -42,35 +42,35 @@ ALTER TABLE FactSalesTransactions ALTER COLUMN order_date DATE;
 DELETE FROM FactSalesTransactions WHERE YEAR(order_date) != 2019;
 
 -- Get distinct count of transactions made
-SELECT COUNT(DISTINCT [order_id]) AS [Transaction Count]
+SELECT COUNT(DISTINCT order_id) AS 'Transaction Count'
 FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions]; --> There are 178,406 records with unique order ID
 
 -- Descriptive Stats of Sales
 WITH cte_price AS
 (
 	SELECT 
-		YEAR([order_date]) AS [year],
-		[order_date],
-		[order_id],
-		[product_quantity] * [product_price] AS [Total Price]
-	FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-	LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-		ON [sales].[product_id] = [prod].[product_id]
-		AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-	WHERE YEAR([order_date]) = 2019
+		YEAR(order_date) AS 'Year',
+		order_date,
+		order_id,
+		product_quantity * product_price AS 'Total Price'
+	FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+	LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+		ON sales.product_id = prod.product_id
+		AND sales.order_date BETWEEN effective_start_date AND effective_end_date
+	WHERE YEAR(order_date) = 2019
 ), 
 -- Median
 cte_median AS
 ( 
 	SELECT
-		(SELECT DISTINCT [year] FROM [cte_price]) AS [Year], 
+		(SELECT DISTINCT year FROM cte_price) AS 'Year', 
 		(
 			(	
 				SELECT MAX([Total Price])
 				FROM 
 				(
 					SELECT TOP 50 PERCENT [Total Price]
-					FROM [cte_price]
+					FROM cte_price
 					ORDER BY 1
 				) a
 			)
@@ -80,29 +80,29 @@ cte_median AS
 				FROM
 				(
 					SELECT TOP 50 PERCENT [Total Price]
-					FROM [cte_price]
+					FROM cte_price
 					ORDER BY 1 DESC
 				) a
 			)
-		) / 2 AS [Median]  --> The median value of single transactions is USD 16.99
+		) / 2 AS median  --> The median value of single transactions is USD 16.99
 ),
 cte_stats AS 
 (
 	SELECT 
-		YEAR([order_date]) AS [Year],
-		[order_id],
-		[product_quantity] * [product_price] AS [Total Price]
-	FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-	LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-		ON [sales].[product_id] = [prod].[product_id]
-		AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-	WHERE YEAR([order_date]) = 2019
+		YEAR(order_date) AS 'Year',
+		order_id,
+		product_quantity * product_price AS 'Total Price'
+	FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+	LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+		ON sales.product_id = prod.product_id
+		AND sales.order_date BETWEEN effective_start_date AND effective_end_date
+	WHERE YEAR(order_date) = 2019
 )
 
 SELECT 
 	a.Year,
 	MIN([Total Price]) AS 'Min Sales (in USD)',
-	MAX([Median]) AS 'Median (in USD)',
+	MAX(median) AS 'Median (in USD)',
 	AVG([Total Price]) AS 'Avg Sales (in USD)',
 	MAX([Total Price]) AS 'Max Sales (in USD)',
 	MAX([Total Price]) - MIN([Total Price]) AS 'Range'
@@ -112,84 +112,56 @@ GROUP BY a.Year;
 
 -- Total Sales in 2019
 SELECT 
-	YEAR([order_date]) AS [Year], 
-	SUM(product_quantity * product_price) AS [Total Sales (in USD)]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-GROUP BY YEAR([order_date]); --> There is a total of sales amount of USD 34,996,944.4699568
+	YEAR(order_date) AS 'Year', 
+	SUM(product_quantity * product_price) AS 'Total Sales (in USD)'
+FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+	ON sales.product_id = prod.product_id
+	AND sales.order_date BETWEEN effective_start_date AND effective_end_date
+GROUP BY YEAR(order_date); --> There is a total of sales amount of USD 34,996,944.4699568
 
 -- Total Qty Sold in 2019
 SELECT 
-	YEAR([order_date]) AS [Year], 
-	SUM(product_quantity) AS [Total Quantity Sold (in pcs)]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-GROUP BY YEAR([order_date]); --> There is a total of quantity sold of 209,038 pcs
+	YEAR(order_date) AS 'Year', 
+	SUM(product_quantity) AS 'Total Quantity Sold (in pcs)'
+FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+GROUP BY YEAR(order_date); --> There is a total of quantity sold of 209,038 pcs
 
 -- Monthly Sales & Qty Sold Trend
 SELECT	
-	YEAR([sales].[order_date]) AS [Year],
-	MONTH([sales].[order_date]) AS [Month],
-	SUM(product_quantity * product_price) AS [Total Sales (in USD)],
-	SUM(product_quantity) AS [Total Quantity Sold (in pcs)]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-GROUP BY YEAR([sales].[order_date]), MONTH([sales].[order_date])
+	YEAR(sales.order_date) AS 'Year',
+	MONTH(sales.order_date) AS 'Month',
+	SUM(product_quantity * product_price) AS 'Total Sales (in USD)',
+	SUM(product_quantity) AS 'Total Quantity Sold (in pcs)'
+FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+	ON sales.product_id = prod.product_id
+	AND sales.order_date BETWEEN effective_start_date AND effective_end_date
+GROUP BY YEAR(sales.order_date), MONTH(sales.order_date)
 ORDER BY 2;
 
 -- Daily Sales & Qty Sold Trend
 SELECT
-	[sales].[order_date] AS [Order Date],
-	SUM(product_quantity * product_price) AS [Total Sales (in USD)],
-	SUM(product_quantity) AS [Total Quantity Sold (in pcs)]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-GROUP BY [sales].[order_date]
+	sales.order_date AS 'Order Date',
+	SUM(product_quantity * product_price) AS 'Total Sales (in USD)',
+	SUM(product_quantity) AS 'Total Quantity Sold (in pcs)'
+FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+	ON sales.product_id = prod.product_id
+	AND sales.order_date BETWEEN effective_start_date AND effective_end_date
+GROUP BY sales.order_date
 ORDER BY 1;
 
 -- Top 5 Daily Sales
 SELECT TOP 5
-	[sales].[order_date] AS [Order Date],
-	SUM(product_quantity * product_price) AS [Total Sales (in USD)],
-	SUM(product_quantity) AS [Total Quantity Sold (in pcs)]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-GROUP BY [sales].[order_date]
-ORDER BY 2 DESC;
-
--- Distinct count of product
-SELECT COUNT(DISTINCT product_id) AS [Product Count]
-FROM [FictionalSalesDatabase].[dbo].[products]; -- There are 19 records of unique products being sold in the store
-
--- Top 5 Product by Qty
-SELECT TOP 5
-	[prod].[product_name],
-	SUM(product_quantity * product_price) AS [Total Sales (in USD)],
-	SUM(product_quantity) AS [Total Quantity Sold (in pcs)]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-GROUP BY [prod].[product_name]
-ORDER BY 3 DESC;
-
--- Top 5 Product by Sales
-SELECT TOP 5
-	[prod].[product_name],
-	SUM(product_quantity * product_price) AS [Total Sales (in USD)],
-	SUM(product_quantity) AS [Total Quantity Sold (in pcs)]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [effective_start_date] AND [effective_end_date]
-GROUP BY [prod].[product_name]
+	sales.order_date AS 'Order Date',
+	SUM(product_quantity * product_price) AS 'Total Sales (in USD)',
+	SUM(product_quantity) AS 'Total Quantity Sold (in pcs)'
+FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+	ON sales.product_id = prod.product_id
+	AND sales.order_date BETWEEN effective_start_date AND effective_end_date
+GROUP BY sales.order_date
 ORDER BY 2 DESC;
 
 -- Distinct count of customer
@@ -202,49 +174,91 @@ FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions]; --> There are 140,7
 
 -- Top 5 Customers
 SELECT TOP 5
-	[cust].[cust_id],
-	SUM([product_quantity] * [product_price]) AS 'Total Spending in 2019 (in USD)'
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-RIGHT JOIN [FictionalSalesDatabase].[dbo].[DimCustomer] [cust] 
-	ON [sales].[cust_id] = [cust].[cust_id]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [prod].[effective_start_date] AND [prod].[effective_end_date]
-GROUP BY [cust].[cust_id]
+	cust.cust_id,
+	ROUND(SUM(product_quantity * product_price), 2) AS 'Total Spending in 2019 (in USD)'
+FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+RIGHT JOIN [FictionalSalesDatabase].[dbo].[DimCustomer] cust
+	ON sales.cust_id = cust.cust_id
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+	ON sales.product_id = prod.product_id
+	AND sales.order_date BETWEEN prod.effective_start_date AND prod.effective_end_date
+GROUP BY cust.cust_id
 ORDER BY 2 DESC;
 
 -- Customer Age Distribution
 SELECT 
-	[cust_age],
-	COUNT([cust_id]) AS 'Frequency'
+	cust_age,
+	COUNT(cust_id) AS 'Frequency'
 FROM [FictionalSalesDatabase].[dbo].[DimCustomer]
-GROUP BY [cust_age]
+GROUP BY cust_age
 ORDER BY 1;
 
 -- Customer Age Stats
+WITH cte_median AS
+( 
+	SELECT
+	(
+		SELECT MAX(cust_age)
+		FROM 
+		(
+			SELECT TOP 50 PERCENT cust_age
+			FROM [FictionalSalesDatabase].[dbo].[DimCustomer]
+			ORDER BY 1
+		) a
+	)
+	+
+	(
+		SELECT MIN(cust_age)
+		FROM
+		(
+			SELECT TOP 50 PERCENT cust_age
+			FROM [FictionalSalesDatabase].[dbo].[DimCustomer]
+			ORDER BY 1 DESC
+		) a
+	) / 2 AS median  --> The median value of customer age is 66 years old
+)
+
+SELECT
+	MIN(cust_age) AS 'Min Age of Customer',
+	(SELECT median FROM cte_median) AS 'Median Age of Customer',
+	AVG(cust_age) AS 'Avg Age of Customer',
+	MAX(cust_age) AS 'Max Age of Customer',
+	MAX(cust_age) - MIN(cust_age) AS 'Range'
+FROM [FictionalSalesDatabase].[dbo].[DimCustomer];
 
 -- Customer Demographic (City) Distribution
 SELECT 
-	[cust_city],
-	COUNT([cust_id]) AS 'Frequency'
+	cust_city,
+	COUNT(cust_id) AS 'Number of Unique Customer'
 FROM [FictionalSalesDatabase].[dbo].[DimCustomer]
-GROUP BY [cust_city]
+GROUP BY cust_city
 ORDER BY 2 DESC;
 
 -- Top Spending by Customer City
+SELECT 
+	cust_city,
+	ROUND(SUM(sales.product_quantity * prod.product_price), 2) AS 'Total Spending (in USD)'
+FROM [FictionalSalesDatabase].[dbo].[DimCustomer] cust
+JOIN [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales 
+	ON cust.cust_id = sales.cust_id
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod 
+	ON sales.product_id = prod.product_id
+	AND sales.order_date BETWEEN prod.effective_start_date AND prod.effective_end_date
+GROUP BY cust_city
+ORDER BY 2 DESC;
 
 -- Data Overview
 SELECT TOP 10
-    [order_date],
-	  [sales].[cust_id],
-	  [product_name],
-    [product_quantity],
-	  [prod].[product_price],
-	  [cust].[cust_city]
-FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] [sales]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] [prod]
-	ON [sales].[product_id] = [prod].[product_id]
-	AND [sales].[order_date] BETWEEN [prod].[effective_start_date] AND [prod].[effective_end_date]
-LEFT JOIN [FictionalSalesDatabase].[dbo].[DimCustomer] [cust] 
-	ON [sales].[cust_id] = [cust].[cust_id]
-ORDER BY [order_date];
+	order_date,
+	sales.cust_id,
+	product_name,
+	product_quantity,
+	prod.product_price,
+	cust.cust_city
+FROM [FictionalSalesDatabase].[dbo].[FactSalesTransactions] sales
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimProduct] prod
+	ON sales.product_id = prod.product_id
+	AND sales.order_date BETWEEN prod.effective_start_date AND prod.effective_end_date
+LEFT JOIN [FictionalSalesDatabase].[dbo].[DimCustomer] cust
+	ON sales.cust_id = cust.cust_id
+ORDER BY order_date;
